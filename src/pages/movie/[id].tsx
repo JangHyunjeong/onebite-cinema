27f934d1f6eea251e87569a69a7c5674861e691a
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2025-06-24 23:53:00 +0900

    feat: 초기 페이지 셋팅

---
+++ src/pages/movie/[id].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function Movie() {
+  const router = useRouter();
+  const { id = "" } = router.query;
+
+  return (
+    <>
+      <h2>{id} 영화 상세 페이지</h2>
+    </>
+  );
+}

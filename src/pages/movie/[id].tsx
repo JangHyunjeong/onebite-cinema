5c3699f3963d63cb459bc5827d68806eef8eef72
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2025-06-30 23:14:12 +0900

    style: movie 스타일링 및 더미데이터 추가

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,12 +1,42 @@
 import { useRouter } from "next/router";
+import style from "./[id].module.css";
+import movieData from "@/mock/dummy.json";
+import type { MovieDataType } from "@/type";
 
 export default function Movie() {
   const router = useRouter();
-  const { id = "" } = router.query;
-
-  return (
-    <>
-      <h2>{id} 영화 상세 페이지</h2>
-    </>
+  const { id } = router.query;
+  const movie = (movieData as MovieDataType[]).find(
+    (item) => String(id) === String(item.id)
   );
+
+  if (!movie) return <>데이터 오류!</>;
+  else {
+    return (
+      <>
+        <div
+          className={style.thumbWrap}
+          style={
+            {
+              "--poster-img-url": `url(${movie.posterImgUrl})`,
+            } as React.CSSProperties
+          }
+        >
+          <img src={movie.posterImgUrl} alt={movie.title} />
+        </div>
+        <div className={style.content}>
+          <p className={style.title}>{movie.title}</p>
+          <div className={style.infos}>
+            <p>
+              {movie.releaseDate} / {movie.genres} / {movie.runtime}분
+            </p>
+            <p>{movie.company}</p>
+          </div>
+
+          <p className={style.subTitle}>{movie.subTitle}</p>
+          <p className={style.desc}>{movie.description}</p>
+        </div>
+      </>
+    );
+  }
 }
